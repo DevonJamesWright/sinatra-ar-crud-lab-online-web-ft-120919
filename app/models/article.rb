e0ed45f22e044c8360730703cb533015648f060d
DevonJamesWright <devonjamess.wright@gmail.com>
2020-02-14 13:03:17 -0500

    Done.

--- app/models/article.rb
+++ app/models/article.rb
@@ -1,4 +1,14 @@
 #Placeholder for a model
 
 class Article < ActiveRecord::Base
+    # @@all = []
+    # attr_reader :title, :content
+    # def initialize(params)
+    #     @@all << self
+    # end
+    # def self.all
+    # @@all
+    
+    # end 
+    
 end
